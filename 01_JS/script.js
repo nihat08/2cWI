8fbe30e4e2dd3b4d9f6bdadef6c49b7929a9c639
nihat_08 <nihat.oezgoec@student.htldornbirn.at>
2023-11-23 09:16:29 +0100

    01_JS_finished

---
+++ 01_JS/script.js
@@ -0,0 +1,29 @@
+console.log("Hallo");
+console.log("Tolle Sache");
+// Variablen
+let a = 7;
+let b = 8;
+let c = 10;
+// Berechne folgendes und gib das Eregebnis über console.log aus
+// a + b + c
+let result = a + b +c;
+console.log(result);
+// (a + b) / c
+let result2 = (a + b) / c
+console.log(result2);
+// c -a + b
+let result3 = c - a + b
+console.log(result3);
+// a + b + c * 3
+let result4 = a + b + c * 3
+console.log(result4);
+// (a + b + c)/4
+let result5 = (a + b + c) / 4
+console.log(result5);
+// Ändere die Variablen auf folgende Werte
+// a = 3
+a = 3
+// b = a + 3
+b = a + 3
+c = c -3
+// gib die neuen Werte der Variablen aus
