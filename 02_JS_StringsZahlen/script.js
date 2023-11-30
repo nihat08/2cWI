785bbf3c5685c5466de1eb55f2fa35e6addf1034
nihat_08 <nihat.oezgoec@student.htldornbirn.at>
2023-11-30 08:28:19 +0100

    02_JS_StringsZahlen_finished

---
+++ 02_JS_StringsZahlen/script.js
@@ -0,0 +1,12 @@
+//Berechne das Ergebnis von 17.4 / 3.9. Gib folgenden Satz aus:
+//The result of 17.4 divided by 3.9 is 4.461538461538462
+let a = 17.4;
+let b = 3.9;
+let result = a / b;
+console.log("Das Ergebnis von" + a + b + "ist" + result);
+
+//Lege eine Variable firstname und eine Varialbe lastname an. Weise den Variablen Deinen Namen zu. Gib folgendes aus:
+"Ich heiße VORNAME NACHNAME"
+
+console.log("Ich heiße Nihat Özgöc");
+
