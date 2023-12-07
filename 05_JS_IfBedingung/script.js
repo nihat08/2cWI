8a295b376dcd1ee035af331cd361bc624aa9531e
nihat_08 <nihat.oezgoec@student.htldornbirn.at>
2023-12-07 09:21:45 +0100

    05_JS_If_finished

--- 05_JS_IfBedingung/script.js
+++ 05_JS_IfBedingung/script.js
@@ -1,5 +1,5 @@
 // erstelle eine Variable number
-let number = 37;
+let number = 57;
 
 // Aufgabe 1
 // Wenn die Zahl kleiner ist als 20  gib aus "Mini"
@@ -20,3 +20,13 @@ else if(number>50) {
 // Wenn die Zahl kleiner gleich 7 ist gib aus "Mini"
 // Wenn die Zahl zwischen 7 und 90 ist gib  aus "Medium"
 // Wenn die Zahl größer als 90 ist gib aus "Large"
+
+if (number<=7){
+    console.log("Mini");
+}
+else if (number>7 && number<90){
+    console.log("Medium");
+}
+else if (number>90){
+    console.log("Large");
+}
