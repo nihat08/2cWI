90bb426e7b4d68b86e3ae2d321cd368563a18ffd
nihat_08 <nihat.oezgoec@student.htldornbirn.at>
2023-12-07 08:58:26 +0100

    04_JSSchleifen_FirstPart

---
+++ 04_JSSchleifen/script.js
@@ -0,0 +1,25 @@
+// Zähle von 7 bis 45
+// Zähle von 9 bis 99
+// Zähle rückwärts von 10 bis 0
+// Zähle rückwärts von 1234 bis 1207
+
+// Gib Deinen Namen genau 8 mal aus
+// Zähle von 0 auf 10, dann gib Deinen Namen aus, dann zähle von 10 auf 0
+
+for (let i = 7; i < 46; i++) {
+    console.log(i);
+}
+
+for (let i = 9; i < 100; i++) {
+    console.log(i);    
+}
+
+for (let i = 11; i >= 0; i--) {
+    console.log(i);    
+}
+
+for (let i = 1235; i >= 1207; i--) {
+    console.log(i);    
+}
+
+
