4f31ebca96b693e3847e485a792f10ea36f8dcc5
nihat_08 <nihat.oezgoec@student.htldornbirn.at>
2023-12-14 09:24:32 +0100

    07_LogischeOperatoren_finished

--- 07_LogischeOperatoren/script.js
+++ 07_LogischeOperatoren/script.js
@@ -12,8 +12,11 @@ if (random1<random2 && random1<50){
 }
 // Wenn die erste Zahl kleiner ist als 30 oder die zweite Zahl kleiner ist als 30
 // dann gib aus "Eine der beiden ist kleiner als 30"
-if (random1<30 || random2<30){
+else if (random1<30 || random2<30){
     console.log("Eine der beiden ist kleiner als 30");
 }
 // Wenn die erste Zahl kleiner ist als 50 UND die zweite Zahl ungleich 50 ist
 // dann gib aus "Erste Zahl klein, zweite kein 50iger"
+else if (random1<50 && random2!==50){
+    console.log("Erste Zahl klein, zweite kein 50iger");
+}
