61ff6005d1bf95e5de4771a209ef253ac19a3025
nihat_08 <nihat.oezgoec@student.htldornbirn.at>
2024-01-02 19:49:42 +0100

    08_SwitchStatement_finished

--- 08_SwitchStatement/script.js
+++ 08_SwitchStatement/script.js
@@ -1,5 +1,5 @@
 // Erstelle eine Zufallszahl zwischen 5 und 10
-let random1 = Math.random () *5 + 5;
+let random1 = Math.random () *5 + 6;
 console.log(random1);
 // Wenn der Wert 10 ist gib aus Ten
 if (random1>10 && random1<11) {
