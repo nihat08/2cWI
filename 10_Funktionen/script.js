90b45c40a634a1ab05275ffbfafdfbbd556373ab
nihat_08 <nihat.oezgoec@student.htldornbirn.at>
2024-01-18 08:38:17 +0100

    10_Funktionen_RechnungenFertig

--- 10_Funktionen/script.js
+++ 10_Funktionen/script.js
@@ -9,10 +9,26 @@ let a = 7;
 let b = 4;
 
 function add(a,b){
-    return a + b 
-}
+    return a + b
+}   
+let result = add(a,b);
+console.log(result);
+
 function subtract(a,b){
     return a - b
 }
+let result2 = subtract(a,b);
+console.log(result2);
 
+function mulitply(a,b){
+    return a * b
+}
+let result3 = mulitply(a,b);
+console.log(result3);
+
+function supercalculation(a,b){
+    return (a+b)/2*a
+}
+let result4 = supercalculation(a,b);
+console.log(result4);
 
