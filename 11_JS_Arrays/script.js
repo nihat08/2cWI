a96eb4a8c240b8b2cc762dae1c88d85e323dfe1c
nihat_08 <nihat.oezgoec@student.htldornbirn.at>
2024-01-29 17:35:23 +0100

    11_JS_Arrays_A2_Frage

--- 11_JS_Arrays/script.js
+++ 11_JS_Arrays/script.js
@@ -1,3 +1,4 @@
+//Aufgabe 1
 let a = 4;
 let b = 1;
 let c = 2;
@@ -17,3 +18,16 @@ console.log("Summe =", a+b+c+d+17+199);
 
 let Summe = (226);
 console.log("Mittelwert =", Summe/6);
+
+//Aufgabe 2
+let x = "Susi";
+let y = "Paula";
+let z = "Hans";
+let array2 = [x,y,"und", z];
+
+for (i=0; i<array2.length; i++) {
+    console.log("Meine Freunde sind" + array2[i]);
+}
+console.log("Meine Freunde sind " + array2);
+array2.push("Sepp");
+console.log("Meine Freunde sind " + );
