509f4ec0121477d508633e8c5d482eac00b10e12
nihat_08 <nihat.oezgoec@student.htldornbirn.at>
2024-04-04 08:24:58 +0200

    bankomat teilweise fertig

---
+++ 11_JS_Arrays/array.js
@@ -0,0 +1,21 @@
+//Aufgabe 1
+	//Erstelle ein Array mit folgenden Werten:
+	//4,1,2,3
+	//Füge die Werte 17 und 199 dazu
+	let array =[4,1,2,3];
+    array.push =[17,199];
+    console.log(array + "," + array.push);
+	//	a. Gib alle Werte in einer Schleife aus!
+	//	b. Zähle alle Werte zusammen und gib das Ergebnis aus
+	//	c. Berechne den Mittelwert der Zahlen und gib das Ergebnis aus
+
+
+//Aufgabe 2
+//	Erstelle ein Array mit folgenden Strings:
+//	"Susi", "Paula", "Hans"
+//	
+//	Gib folgenden Satz aus:
+//	"Meine Freunde sind Susi, Paula und Hans
+//	
+//	Füge "Sepp" dazu. Nun soll die ausgabe folgendermaßen lauten:
+//	"Meine Freunde sind Susi, Paula, Hans und Sepp"
--- test.js
+++ test.js
@@ -15,8 +15,9 @@ const readLineAsync =() => {
 };
 
 while(true){
-    
-    console.log("Wie viel möchten Sie einzahlen?");
+    async function welcheeingabe(eingabe) {
+    if (eingabe === "1") {
+        console.log("Wie viel möchten Sie einzahlen?");
     let selection = await readLineAsync();
     console.log("Sie haben" + selection + "€ eingezahlt");
     switch (selection) {
@@ -27,36 +28,13 @@ while(true){
         default:
             break;
     }
-    
-    console.log("Wie viel möchten Sie abheben?");
-    let selection2 = await readLineAsync();
-    console.log("Sie haben" + selection2 + "€ abgehoben");
-    switch (selection2) {
-        case 1:
-            
-            break;
-    
-        default:
-            break;
+    }
     }
 
-    console.log("Wie viel haben Sie auf dem Konto?");
-    let selection3 = (selection - selection2);
-    console.log("Sie haben" + selection3 + "€ auf dem Konto");
-    switch (selection3) {
-        case 1:
-            
-            break;
-    
-        default:
-            break;
-    }
-
-}
 
 console.log(name);
 
 let age = await readLineAsync();
 console.log(age);
 
-readLine.close();
+readLine.close();}
