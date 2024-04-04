5a5da091bca957796211db4d2f6de04785f644ee
nihat_08 <nihat.oezgoec@student.htldornbirn.at>
2024-04-04 09:32:08 +0200

    TTT bisschen und Bankomat test

--- 13_Bankomat/test.js
+++ 13_Bankomat/test.js
@@ -13,8 +13,8 @@ const readLineAsync = () => {
     });
   });
 };
-
-while (true) {
+//input muss noch definiert werden
+if (input == 1) {
   console.log("Wie viel möchten Sie einzahlen?");
   let selection = await readLineAsync();
   console.log("Sie haben" + selection + "€ eingezahlt");
@@ -25,21 +25,11 @@ while (true) {
     default:
       break;
   }
-
+} else if (input == 2) {
   console.log("Wie viel möchten Sie abheben?");
   let selection2 = await readLineAsync();
   console.log("Sie haben" + selection2 + "€ abgehoben");
   switch (selection2) {
-    case 1:
-      break;
-
-    default:
-      break;
-  }
-
-  let selection3 = selection - selection2;
-  console.log("Sie haben" + selection3 + "€ auf dem Konto");
-  switch (selection3) {
     case 1:
       break;
 
