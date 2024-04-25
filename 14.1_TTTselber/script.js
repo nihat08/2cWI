429f6c016f1316917376bd9a5df86f03d50255f1
nihat_08 <nihat.oezgoec@student.htldornbirn.at>
2024-04-25 08:01:30 +0200

    .

--- 14.1_TTTselber/script.js
+++ 14.1_TTTselber/script.js
@@ -14,3 +14,56 @@ const winConditions = [
   [2, 4, 6],
 ];
 let options = ["", "", "", "", "", "", "", "", ""];
+let currentPlayer = "X";
+let running = false;
+
+initializeGame();
+function initializeGame() {
+  cells.forEach((cell) => cell.addEventListener("click", cellClicked));
+  restartButton.addEventListener("click", restartGame);
+  statusText.textContent = `${currentPlayer}'s turn`;
+}
+function cellClicked() {
+  const cellIndex = this.getAttribute("cellIndex");
+
+  if (options[cellIndex] != "" || !running) {
+    return;
+  }
+
+  updateCell(this, cellIndex);
+  checkWinner();
+}
+function updateCell(cell, index) {
+  options[index] = currentPlayer;
+  cell.textContent = currentPlayer;
+}
+function changePlayer() {
+  currentPlayer = currentPlayer == "X" ? "0" : "X";
+  statusText.textContent = `${currentPlayer}'s turn`;
+}
+function checkWinner() {
+  let roundWon = false;
+
+  for (let i = 0; i < winConditions.length; i++) {
+    const condition = winConditions[i];
+    const cellA = options[condition[0]];
+    const cellB = options[condition[1]];
+    const cellC = options[condition[2]];
+
+    if (cellA == "" || cellB == "" || cellC == "") {
+      continue;
+    }
+    if (cellA == cellB && cellB == cellC) {
+      roundWon = true;
+      break;
+    }
+  }
+  if (roundWon) {
+    statusText.textContent = `${currentPlayer} wins!`;
+    running = false;
+  } else if (!options.includes("")) {
+    statusText.textContent = `Draw!`;
+    running = false;
+  }
+}
+function restartGame() {}
