18d5c53f50ec413f681ccf3e68b351c9253d17c5
nihat_08 <nihat.oezgoec@student.htldornbirn.at>
2024-05-16 08:58:50 +0200

    BankomatFinished

--- 13_Bankomat/script.js
+++ 13_Bankomat/script.js
@@ -13,48 +13,30 @@ const readLineAsync = () => {
     });
   });
 };
-
+let balance = 0;
 while (true) {
   console.log("1 = Einzahlen, 2 = Abheben, 3 = Kontostand");
+  console.log("balance:" + balance + " €");
 
   let select1 = await readLineAsync();
   if (select1 == 1) {
     console.log("Wie viel möchten Sie einzahlen?");
-    let selection = await readLineAsync();
-    console.log("Sie haben" + selection + "€ eingezahlt");
-    switch (selection) {
-      case 1:
-        break;
-
-      default:
-        break;
-    }
+    let amount = await readLineAsync();
+    balance += parseInt(amount);
+    console.log("Sie haben" + amount + " € eingezahlt");
   }
-  let select2 = await readLineAsync();
-  if (select2 == 2) {
-    console.log("Wie viel möchten Sie abheben?");
-    let selection2 = await readLineAsync();
-    console.log("Sie haben" + selection2 + "€ abgehoben");
-    switch (selection2) {
-      case 1:
-        break;
 
-      default:
-        break;
-    }
+  if (select1 == 2) {
+    console.log("Wie viel möchten Sie abheben?");
+    let amount = await readLineAsync();
+    balance -= parseInt(amount);
+    console.log("Sie haben" + amount + " € abgehoben");
+  }
+
+  if (select1 == 3) {
+    console.log("Sie haben" + balance + " € auf dem Konto");
   }
 }
-
-//   let selection3 = selection - selection2;
-//   console.log("Sie haben" + selection3 + "€ auf dem Konto");
-//   switch (selection3) {
-//     case 1:
-//       break;
-
-//     default:
-//       break;
-//   }
-// }
 
 let age = await readLineAsync();
 console.log(age);
